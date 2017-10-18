dd3f4412e2956e808beef9fc45efe8f5fe038396
DESKTOP-700H45R\Kmilo <ca.rojasb@uniandes.edu.co>
2017-10-17 20:21:19 -0500

    Ultimo

--- Taller2_WikiFounderWeb/src/java/uniandes/grupo5/servlet/Servlet.java
+++ Taller2_WikiFounderWeb/src/java/uniandes/grupo5/servlet/Servlet.java
@@ -3,7 +3,6 @@ package uniandes.grupo5.servlet;
 import com.jcraft.jsch.*;
 import java.io.*;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -75,8 +74,7 @@ public class Servlet extends HttpServlet {
                         
             String validar = sshConector.executeCommand("if [ -d relaciones_taller2 ]; then echo \"SI\"; else echo \"NO\"; fi");
             String part = null;
-            String texto_json = "";
-            String renglon = "";
+            String original = null;
             
             if (validar.contains("NO")){
                 
@@ -93,30 +91,24 @@ public class Servlet extends HttpServlet {
                         sshConector.executeCommand("hadoop jar Wikibz.jar uniandes.reuters.job.Archivo_Salida_Job relaciones_taller2 salida_taller2 " + hecho.replace(" ", "_") + " " + lugar.replace(" ", "_") + " " + perso.replace(" ", "_") + " " + fecha_inicial.replace(" ", "_") + " " + fecha_final.replace(" ", "_"));
                 sshConector.executeCommand("hadoop fs -get salida_taller2");
                 
-                part = sshConector.executeCommand("cat salida_taller2/part-r-00000");
+                original = sshConector.executeCommand("cat salida_taller2/part-r-00000");
+                part = sshConector.executeCommand("cat salida_taller2/part-r-00000")
+                        .replaceAll("<br>", " ").replaceAll("<br />", " ").replaceFirst("[{]", "{ \"nodes\": [{").replaceAll("[||||]", "\n")
+                        .replaceFirst(", [{]\"so", "], \"links\": [ {\"so").replaceAll("1[{]","{");
+                part = part.substring(0,part.length() - 3) + " ] }";
                 
-                
-                for (int n=0; n<part.length(); n++) {
-                    char c = part.charAt(n);
-                    if (renglon == ""){
-                        texto_json = texto_json + c;
-                    }
-                    System.out.println (c);
-                };
-                
+                //part = "{ \"nodes\": [{\"id\": \"yakima herald-republic\",\"group\": 1}, {\"id\": \"name = yakima ''herald-republic''\",\"group\": 2}, {\"id\": \"image = [[image:yakima herald-republic front page.jpg\",\"group\": 2}, {\"id\": \"caption = the july 27, 2005 front page of the ''yakima herald-republic''\",\"group\": 2}, {\"id\": \"type = daily [[newspaper]]\",\"group\": 2}, {\"id\": \"format = [[broadsheet]]\",\"group\": 2}, {\"id\": \"foundation =\",\"group\": 2}, {\"id\": \"ceased publication =\",\"group\": 2}, {\"id\": \"price =\",\"group\": 2}, {\"id\": \"owners = [[the seattle times company]]\",\"group\": 2}, {\"id\": \"publisher = bob crider\",\"group\": 2}, {\"id\": \"managing editor = alison bath\",\"group\": 2}, {\"id\": \"language = english\",\"group\": 2}, {\"id\": \"circulation =\",\"group\": 2}, {\"id\": \"headquarters = 114 n. 4th street [[yakima, washington\",\"group\": 2}, {\"id\": \"issn =\",\"group\": 2}, {\"id\": \"website = [http://yakimaherald.com yakimaherald.com]\",\"group\": 2}],\"links\": [{\"source\": \"yakima herald-republic\",\"target\": \"name = yakima ''herald-republic''\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"image = [[image:yakima herald-republic front page.jpg\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"caption = the july 27, 2005 front page of the ''yakima herald-republic''\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"type = daily [[newspaper]]\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"format = [[broadsheet]]\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"foundation =\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"ceased publication =\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"price =\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"owners = [[the seattle times company]]\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"publisher = bob crider\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"managing editor = alison bath\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"language = english\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"circulation =\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"headquarters = 114 n. 4th street [[yakima, washington\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"issn =\",\"value\": 1}, {\"source\": \"yakima herald-republic\",\"target\": \"website = [http://yakimaherald.com yakimaherald.com]\",\"value\": 1} ] }";
+                               
             };
             
             
-
-
+            
             
             PrintWriter out = response.getWriter();
             
             out.println("<html>");
-                out.println("<head>");
+                out.println("<head>");                
                     out.println("<title>Respuesta Servlet</title>");
-                    out.println(validar+"\n");
-                    out.println(part);
                     out.println("<link href=\"Resources/css/screen.css\" rel=\"stylesheet\" type=\"text/css\"/>");                                    
                     out.println("<style>\n" +
                                 ".links line {\n" +
@@ -138,7 +130,14 @@ public class Servlet extends HttpServlet {
                     out.println("<h2 style=\"color: black\" align=\"center\"> <b>Fecha inicial = </b>" + fecha_inicial.replace("_", " ")  + "</h2>");
                     out.println("<h2 style=\"color: black\" align=\"center\"> <b>Fecha final = </b>" + fecha_final.replace("_", " ")  + "</h2><BR><BR>");                    
                     //out.println("<h2 style=\"color: black\">El resultado del jar es..." + result + "</h2>");
-                    out.println("<svg width=\"1400\" height=\"600\"></svg>");                    
+                    out.println("<svg width=\"1400\" height=\"600\"></svg> <br>");     
+                    out.println(part + "<br>");
+                    //out.println("<script type=\"text/javascript\" src=\"CargarJson.js\"></script>");
+                    //out.println("<span id=\"res\"> Nada </span>");
+                    /*out.println("<script type=\"text/javascript\">\n" +
+                                "var jsonText = JSON.stringify(\"" + part + "\");" +
+                                "document.write(jsonText);" +
+                                "</script>");*/
                     out.println("<script src=\"https://d3js.org/d3.v4.min.js\"></script>\n" +
                                 "<script>\n" +
                                 "\n" +
@@ -153,9 +152,9 @@ public class Servlet extends HttpServlet {
                                 "    .force(\"charge\", d3.forceManyBody())\n" +
                                 "    .force(\"center\", d3.forceCenter(width / 2, height / 2));\n" +
                                 "\n" +
-                                "d3.json(\"DatosGrafo/flare.json\", function(error, graph) {\n" +
+                                    "d3.json(\"DatosGrafo/flare.json\", function(error, graph) {\n" +
                                 "  if (error) throw error;\n" +
-                                "\n" +
+                                "\n" +                                
                                 "  var link = svg.append(\"g\")\n" +
                                 "      .attr(\"class\", \"links\")\n" +
                                 "    .selectAll(\"line\")\n" +
@@ -220,7 +219,7 @@ public class Servlet extends HttpServlet {
                                 "  d.fy = null;\n" +
                                 "}\n" +
                                 "\n" +
-                                "</script>");
+                                "</script>");                
                 out.println("<p class=\"linkVolver\" align=\"center\">\n" +
                 "<a href=\"index.jsp\" style=\"font-size: 15pt; font-family: Comic Sans MS; color: white; align-items: center\">Inicio</a>\n" +
                 "<br><br>\n" +
