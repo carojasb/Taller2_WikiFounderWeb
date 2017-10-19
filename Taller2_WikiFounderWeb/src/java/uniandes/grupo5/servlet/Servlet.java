92ff1b9a373f233036624085cfe819427bea8480
PABLO-LAPTOP\pablo <pabloandresboada@gmail.com>
2017-10-19 01:02:30 -0500

    Se modifica servlet para que grafique sin depender del archivo json Se crea funcion js

--- Taller2_WikiFounderWeb/src/java/uniandes/grupo5/servlet/Servlet.java
+++ Taller2_WikiFounderWeb/src/java/uniandes/grupo5/servlet/Servlet.java
@@ -130,96 +130,27 @@ public class Servlet extends HttpServlet {
                     out.println("<h2 style=\"color: black\" align=\"center\"> <b>Fecha inicial = </b>" + fecha_inicial.replace("_", " ")  + "</h2>");
                     out.println("<h2 style=\"color: black\" align=\"center\"> <b>Fecha final = </b>" + fecha_final.replace("_", " ")  + "</h2><BR><BR>");                    
                     //out.println("<h2 style=\"color: black\">El resultado del jar es..." + result + "</h2>");
-                    out.println("<svg width=\"1400\" height=\"600\"></svg> <br>");     
-                    out.println(part + "<br>");
-                    //out.println("<script type=\"text/javascript\" src=\"CargarJson.js\"></script>");
-                    //out.println("<span id=\"res\"> Nada </span>");
-                    /*out.println("<script type=\"text/javascript\">\n" +
-                                "var jsonText = JSON.stringify(\"" + part + "\");" +
-                                "document.write(jsonText);" +
-                                "</script>");*/
+                    out.println("<svg width=\"960\" height=\"600\"></svg><br>");                    
                     out.println("<script src=\"https://d3js.org/d3.v4.min.js\"></script>\n" +
+                            
+                            
+                                "<script type=\"text/javascript\" src=\"js/dibujar_grafo.js\"></script>\n" +
                                 "<script>\n" +
                                 "\n" +
-                                "var svg = d3.select(\"svg\"),\n" +
-                                "    width = +svg.attr(\"width\"),\n" +
-                                "    height = +svg.attr(\"height\");\n" +
+                                "var nodos_rel = { \n" +
+                                "    \"nodes\": [ \n" +
+                                "    {\"id\": \"Ricky Minard\",\"group\": 1},\n" +
+                                "    {\"id\": \"birth_place  = [[Mansfield, Ohio]]\",\"group\": 2}  ], \n" +
+                                "    \"links\": [    \n" +
+                                "     {\"source\": \"Ricky Minard\",\"target\": \"birth_place  = [[Mansfield, Ohio]]\",\"value\": 1}    \n" +
+                                "    ]};   \n" +
                                 "\n" +
-                                "var color = d3.scaleOrdinal(d3.schemeCategory20);\n" +
-                                "\n" +
-                                "var simulation = d3.forceSimulation()\n" +
-                                "    .force(\"link\", d3.forceLink().id(function(d) { return d.id; }))\n" +
-                                "    .force(\"charge\", d3.forceManyBody())\n" +
-                                "    .force(\"center\", d3.forceCenter(width / 2, height / 2));\n" +
-                                "\n" +
-                                    "d3.json(\"DatosGrafo/flare.json\", function(error, graph) {\n" +
-                                "  if (error) throw error;\n" +
-                                "\n" +                                
-                                "  var link = svg.append(\"g\")\n" +
-                                "      .attr(\"class\", \"links\")\n" +
-                                "    .selectAll(\"line\")\n" +
-                                "    .data(graph.links)\n" +
-                                "    .enter().append(\"line\")\n" +
-                                "      .attr(\"stroke-width\", function(d) { return Math.sqrt(d.value); });\n" +
-                                "\n" +
-                                "  var node = svg.append(\"g\")\n" +
-                                "      .attr(\"class\", \"nodes\")\n" +
-                                "    .selectAll(\"circle\")\n" +
-                                "    .data(graph.nodes)\n" +
-                                "    .enter().append(\"circle\")\n" +
-                                "      .attr(\"r\", 6)\n" +
-                                "      .attr(\"fill\", function (d, i) { \n" +
-                                "			if (d.group==2){\n" +
-                                "				return \"red\" ;\n" +
-                                "		    }else{ return color(d.group);}\n" +
-                                "			}) \n" +
-                                "      .call(d3.drag()\n" +
-                                "          .on(\"start\", dragstarted)\n" +
-                                "          .on(\"drag\", dragged)\n" +
-                                "          .on(\"end\", dragended));\n" +
-                                "\n" +
-                                "  node.append(\"title\")\n" +
-                                "      .text(function(d) { return d.id; });\n" +
-                                "\n" +
-                                "  simulation\n" +
-                                "      .nodes(graph.nodes)\n" +
-                                "      .on(\"tick\", ticked);\n" +
-                                "\n" +
-                                "  simulation.force(\"link\")\n" +
-                                "      .links(graph.links);\n" +
-                                "\n" +
-                                "  function ticked() {\n" +
-                                "    link\n" +
-                                "        .attr(\"x1\", function(d) { return d.source.x; })\n" +
-                                "        .attr(\"y1\", function(d) { return d.source.y; })\n" +
-                                "\n" +
-                                "        .attr(\"x2\", function(d) { return d.target.x; })\n" +
-                                "        .attr(\"y2\", function(d) { return d.target.y; });\n" +
-                                "\n" +
-                                "    node\n" +
-                                "        .attr(\"cx\", function(d) { return d.x; })\n" +
-                                "        .attr(\"cy\", function(d) { return d.y; });\n" +
-                                "  }\n" +
-                                "});\n" +
-                                "\n" +
-                                "function dragstarted(d) {\n" +
-                                "  if (!d3.event.active) simulation.alphaTarget(0.3).restart();\n" +
-                                "  d.fx = d.x;\n" +
-                                "  d.fy = d.y;\n" +
-                                "}\n" +
-                                "\n" +
-                                "function dragged(d) {\n" +
-                                "  d.fx = d3.event.x;\n" +
-                                "  d.fy = d3.event.y;\n" +
-                                "}\n" +
-                                "\n" +
-                                "function dragended(d) {\n" +
-                                "  if (!d3.event.active) simulation.alphaTarget(0);\n" +
-                                "  d.fx = null;\n" +
-                                "  d.fy = null;\n" +
-                                "}\n" +
-                                "\n" +
-                                "</script>");                
+                            
+                                "dibujar_grafo(nodos_rel); \n" +
+                            
+                                "</script>");  
+                    
+                    
                 out.println("<p class=\"linkVolver\" align=\"center\">\n" +
                 "<a href=\"index.jsp\" style=\"font-size: 15pt; font-family: Comic Sans MS; color: white; align-items: center\">Inicio</a>\n" +
                 "<br><br>\n" +
